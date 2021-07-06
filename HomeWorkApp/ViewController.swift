75e5f52f812b696cfa350585d4531ab860fc367e
rkateev <60319058+rkateev@users.noreply.github.com>
2021-07-06 17:37:54 +0300

    Initial Commit

---
+++ HomeWorkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkApp
+//
+//  Created by  Husein on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
